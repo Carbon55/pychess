816e8f8eb766f2383d6a457b42f8a669e1bf7c75
Colin <45411186+Carbon55@users.noreply.github.com>
2021-06-26 11:55:51 -0700

    Added Objects Instead of Strings
    Added Federal Helium Reserve UFO report.

--- board.py
+++ board.py
@@ -22,16 +22,20 @@ class chessgame(object):
         bp7 = pieces.pawn("g","7",-1)
         bp8 = pieces.pawn("h","7",-1)
 
+#['br','bn','bb','bq','bk','bb','bn','br'],
+
+
+#['wr','wn','wb','wq','wk','wb','wn','wr']]
 
         self.board = [
-                 ['br','bn','bb','bq','bk','bb','bn','br'],
+                 [None,None,None,None,None,None,None,None],
                  [bp1,bp2,bp3,bp4,bp5,bp6,bp7,bp8],
-                 [' ',' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' ',' '],
-                 [' ',' ',' ',' ',' ',' ',' ',' '],
+                 [None,None,None,None,None,None,None,None],
+                 [None,None,None,None,None,None,None,None],
+                 [None,None,None,None,None,None,None,None],
+                 [None,None,None,None,None,None,None,None],
                  [wp1,wp2,wp3,wp4,wp5,wp6,wp7,wp8],
-                 ['wr','wn','wb','wq','wk','wb','wn','wr']]
+                 [None,None,None,None,None,None,None,None]]
 
 
 
@@ -69,7 +73,12 @@ class chessgame(object):
             # hint: Magic pre-row
             print(7-i+1,end='|')
             for j in range(0,8):
-                piece = self.board[i][j].id
-                print(dictionary[piece],end='|')
+                piece = self.board[i][j]
+                if piece != None:
+                    print(dictionary[piece.id],end='|')
+                else:
+                    print("   ",end='|')
+
+
             #hint : Magic post-row
             print("")
